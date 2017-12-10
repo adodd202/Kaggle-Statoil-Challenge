821e79ae8d84215237a5b4fdd648c735dbd8d993
Andrew <adodd202@gmail.com>
2017-12-10 11:44:52 -0800

    ee

--- Kaggle-PyTorch/PyTorch-Ensembler/main-statoil.py
+++ Kaggle-PyTorch/PyTorch-Ensembler/main-statoil.py
@@ -152,7 +152,8 @@ def BinaryTrainAndValidate(model, criterion, optimizer, runId, debug=False):
         #Early stopping
         print ('eval_loss', (eval_loss / (len(testset))))
         if epoch > 10:
-            if (val_losses[epoch] > sum(val_losses[epoch-5:epoch-1])/5)
+            if (val_losses[epoch] > (val_losses[epoch-1]+val_losses[epoch-2]+val_losses[epoch-3]+val_losses[epoch-4]  \
+                                    +val_losses[epoch-5])/5)
                 print ("Early stopping (dodd implementation)")
                 break
 
