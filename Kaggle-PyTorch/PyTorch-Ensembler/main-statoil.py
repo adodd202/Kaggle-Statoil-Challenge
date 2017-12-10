527cef157ec6b1a55e3552ee4b4cfb24131fce7d
Andrew <adodd202@gmail.com>
2017-12-09 21:28:16 -0800

    woo

--- Kaggle-PyTorch/PyTorch-Ensembler/main-statoil.py
+++ Kaggle-PyTorch/PyTorch-Ensembler/main-statoil.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
     trainloader, testloader, trainset, testset = loadDB(args)
     for i in tqdm(range(0, 2)):
     #for i in range(0, 51):
-        models = ['senet', 'wrn']
+        models = ['wrn']
         for m in models:
             runId = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
             fixSeed(args)
